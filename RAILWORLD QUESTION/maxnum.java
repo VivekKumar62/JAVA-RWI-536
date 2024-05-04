a3bdac9c71c766f7700adf9c5f0e4587ebe7206c
Vivek Kumar <135468344+VivekKumar62@users.noreply.github.com>
2024-05-04 23:16:18 +0530

    Add files via upload

---
+++ RAILWORLD QUESTION/maxnum.java
@@ -0,0 +1,36 @@
+//import java.util.Scanner;
+//
+//public class maxnum {
+//    public static void main(String[]args){
+//
+//
+//        int []a={1,2,8,80,20};
+//
+//        int max = a[0];
+//        for(int i=1; i<a.length; i++)
+//        {
+//            if(max <a[i]){
+//                max=a[i];
+//
+//            }
+//        }
+//        System.out.println(max);
+//
+//    }
+//}
+
+
+import  java.util.Scanner;
+public class maxnum {
+    public  static  void main(String[]args){
+   int []a={11,20,30,90,};
+   int min=a[0];
+   for(int i=0; i<a.length; i++)
+   {
+       if(min>a[i]){
+           
+           min=a[i];
+       }
+   }
+    }
+}
