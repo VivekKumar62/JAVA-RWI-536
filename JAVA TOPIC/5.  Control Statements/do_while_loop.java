7a3b012874398b5ce72d5dbbc1fce2130fdc5008
Vivek Kumar <135468344+VivekKumar62@users.noreply.github.com>
2024-05-04 18:40:00 +0530

    Add files via upload

---
+++ JAVA TOPIC/5.  Control Statements/do_while_loop.java
@@ -0,0 +1,27 @@
+
+   //  1st  example  of using do while loop , number print 1 to 50
+
+public class do_while_loop {
+    public static void main(String[]args){
+        int i=10;
+
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i<=50);
+    }
+}
+
+     //2nd  example of odd number print
+//     public class do_while_loop {
+//         public static void main(String[] args) {
+//             int i = 1;
+//             do {
+//                 if (i % 2 != 0) {
+//                     System.out.println(i);
+//                 }
+//                 i++;
+//             } while (i <= 100);
+//         }
+//     }
