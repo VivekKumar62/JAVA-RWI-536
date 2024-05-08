67bed95927d17bf714c2d553c637ca0245b45e0e
Vivek Kumar <135468344+VivekKumar62@users.noreply.github.com>
2024-05-08 18:00:41 +0530

    Add files via upload

---
+++ JAVA TOPIC/Collection Framework/collection_sort_custom.java
@@ -0,0 +1,11 @@
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//
+//public class collection_sort_custom {
+//    public static void main(String[] args) {
+//        List<Student> list = new ArrayList<>();
+//        list.add(new Student(name: "vivek", rollNo: 2);
+//        Collections.sort(list);
+//    }
+//}
