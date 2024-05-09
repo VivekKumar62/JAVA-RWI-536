13850ff6f51dddceefe82c75930629a25cc5d57f
Vivek Kumar <135468344+VivekKumar62@users.noreply.github.com>
2024-05-09 22:05:37 +0530

    Add files via upload

---
+++ JAVA TOPIC/JAVA 8/use of comparator/Ascending_Descending.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Ascending_Descending {
+    public static void main(String[] args) {
+
+//        List<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//        list.add(5);
+//        Collections.sort(list,(a,b)-> b-a); //use comparator
+//        System.out.println(list); //[5, 4, 3, 2, 1]
+
+        Set<Integer> s= new TreeSet<>();
+        s.add(1);
+        s.add(69);
+        s.add(39);
+        s.add(48);
+        s.add(59);
+        System.out.println(" before maniual sorting" +s );
+        Set<Integer> ss= new TreeSet<>((a,b)-> b-a);
+        ss.add(22);
+        ss.add(33);
+        ss.add(48);
+        System.out.println("After manial sorting desc: " +ss);
+    }
+}
