e9a8bf57387b7047f48fcc4ca04de06838843cfb
Vivek Kumar <135468344+VivekKumar62@users.noreply.github.com>
2024-05-09 17:42:55 +0530

    Add files via upload

---
+++ JAVA TOPIC/JAVA 8/How to use Lamda Expression/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee =()-> "Software engineer";
+        System.out.println(employee.getName());
+        Employee Trainer = () -> "Ashish Sir and Shivam Sir";
+        System.out.println(Trainer.getName());
+
+
+
+}}
