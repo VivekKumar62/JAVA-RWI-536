b5b676e2645a651762b9146cbeec550839b7494d
Vivek Kumar <135468344+VivekKumar62@users.noreply.github.com>
2024-05-11 02:17:26 +0530

    Add files via upload

---
+++ JAVA TOPIC/JAVA 8/ Function Interface  Use apply(), andThen(), compose() and identity()/function_applyThencomposeidentity.java
@@ -0,0 +1,15 @@
+import java.util.function.Function;
+
+public class function_applyThencomposeidentity {
+    public static void main(String[] args) {
+        Function<Integer,Integer>function1 = x->2*x;
+        Function<Integer,Integer> function2= x->x*x*x;
+        System.out.println(function1.andThen(function2).apply(2)); // 64
+        System.out.println(function2.andThen(function1).apply(3)); //54
+        System.out.println(function1.compose(function2).apply(4)); // 128
+
+        Function<String,String> myidentity = Function.identity();
+        System.out.println(myidentity.apply("Ashish sir") ); //Ashish sir , jo aap inout mai doge
+                                                                // wahi output mai aayega  
+    }
+}
