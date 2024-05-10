ea554c5b2fe865f4aa8aacfb4981c20b4f2a6385
Vivek Kumar <135468344+VivekKumar62@users.noreply.github.com>
2024-05-11 01:16:21 +0530

    Delete JAVA TOPIC/JAVA 8/Example_three_Predicate.java

--- JAVA TOPIC/JAVA 8/Example_three_Predicate.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.Predicate;
-
-public class Example_three_Predicate {
-    public static void main(String[] args) {
-       Predicate<String> startsWithLetterV = x-> x.toLowerCase().charAt(0) == 'v';
-        System.out.println(startsWithLetterV.test("vamika"));
-    }
-}
-//true
