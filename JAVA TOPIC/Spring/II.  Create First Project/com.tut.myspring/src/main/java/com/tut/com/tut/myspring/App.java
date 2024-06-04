7bc210ca66975a7f7cfbdf52bd76ef33af4e3426
Vivek Kumar <135468344+VivekKumar62@users.noreply.github.com>
2024-06-05 03:28:18 +0530

    Add files via upload

---
+++ JAVA TOPIC/Spring/II.  Create First Project/com.tut.myspring/src/main/java/com/tut/com/tut/myspring/App.java
@@ -0,0 +1,20 @@
+package com.tut.com.tut.myspring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main(String[] args)
+    {
+        @SuppressWarnings("resource")
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.cfg.xml");
+        
+        employee1 empl1 = (employee1) context.getBean("employee1");
+        System.out.println(empl1);
+        
+
+        employee1 empl2 = (employee1) context.getBean("student2");
+        System.out.println(empl2);
+    }
+}
